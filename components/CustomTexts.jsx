4d16cc861177e1d8df96852800a7d8d51458a35f
Adit0507 <adi05072002@gmail.com>
2023-02-16 22:00:13 +0530

    Done with Custom text

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,18 @@
 'use client';
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { motion } from 'framer-motion';
+import { textContainer, textVariant2 } from '../utils/motion';
+
+export const TypingText = ({ title, textStyles }) => (
+  <motion.p className={`font-normal text-[14px] text-secondary-white ${textStyles}`} variants={textContainer}>
+    {Array.from(title).map((letter, index) => (
+      <motion.span variants={textVariant2} key={index}>
+        {letter === ' ' ? '\u00A0' : letter}
+      </motion.span>
+    ))}
+  </motion.p>
 );
 
-export const TitleText = () => (
-  <h2>Title Text</h2>
-);
+// export const TitleText = ({ title, textStyles }) => (
+//   <motion.h2 variants={textVariant2} initial="hidden" whileInView="show" className={`mt-[8px] md:text-[64px] ${textStyles}`}>{title}</motion.h2>
+// );
