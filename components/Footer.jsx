1ec52a912288c7197b8c5c8ef679fc17de09944a
Adit0507 <adi05072002@gmail.com>
2023-02-26 00:29:30 +0530

    Styling fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -13,43 +13,43 @@ const Footer = () => (
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="footer-gradient" />
-      <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
-        <div>
-          <h4 className="font-bold md:text-[64px] text-[44px] text-white">
-            Enter the Metaverse
-          </h4>
-          <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
-            <img
-              src="/headset.svg"
-              alt="headset"
-              className="w-[24px] h-[24px] object-contain"
-            />
-            <span className="font-normal text-[16px] text-white">
-              Enter Metaverse
-            </span>
-          </button>
+    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+      <div>
+        <h4 className="font-bold md:text-[64px] text-[44px] text-white">
+          Enter the Metaverse
+        </h4>
+        <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
+          <img
+            src="/headset.svg"
+            alt="headset"
+            className="w-[24px] h-[24px] object-contain"
+          />
+          <span className="font-normal text-[16px] text-white">
+            Enter Metaverse
+          </span>
+        </button>
 
-          <div className="flex flex-col">
-            <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+        <div className="flex flex-col">
+          <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
-            <div className="flex items-center justify-between flex-wrap gap-4">
-              <h4 className="font-extrabold text-[24px] text-white">
-                METAVERSUS
-              </h4>
+          <div className="flex items-center justify-between flex-wrap gap-4">
+            <h4 className="font-extrabold text-[24px] text-white">
+              METAVERSUS
+            </h4>
 
-              <div className="flex gap-4">
-                {socials.map((social) => (
-                  <img
-                    key={social.id}
-                    src={social.url}
-                    className="w-[24px] h-[24px] object-contain cursor-pointer"
-                  />
-                ))}
-              </div>
+            <div className="flex gap-4">
+              {socials.map((social) => (
+                <img
+                  key={social.id}
+                  src={social.url}
+                  className="w-[24px] h-[24px] object-contain cursor-pointer"
+                />
+              ))}
             </div>
           </div>
         </div>
       </div>
+    </div>
   </motion.footer>
 );
 
